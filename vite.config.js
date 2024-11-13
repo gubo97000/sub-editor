b8bc2c7e05f98c16eeb24b35e553afa7b4a085d9
gubo97000 <gubo97000@hotmail.it>
2024-11-13 13:17:18 +0200

    fixed env error

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,18 @@ import fs from 'fs';
 import { vite as vidstack } from 'vidstack/plugins';
 import { defineConfig } from 'vitest/config';
 
+let httpsConfig = process.env.NODE_ENV === "development" ? {
+	key: fs.readFileSync('./cert/key.pem'),
+	cert: fs.readFileSync('./cert/cert.pem')
+} : undefined;
+
 export default defineConfig({
 	plugins: [sveltekit(), vidstack()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
 	server: {
-		https: {
-			key: fs.readFileSync('./cert/key.pem'),
-			cert: fs.readFileSync('./cert/cert.pem')
-		},
-		proxy:{}
+		https: httpsConfig,
+		proxy: {}
 	}
 });
